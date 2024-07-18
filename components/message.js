a00ed8b746eee809b0660f865fc0422b4e58c66e
memoli0 <70433403+memoli0@users.noreply.github.com>
2024-07-18 11:23:19 +0300

    as

--- components/message.js
+++ components/message.js
@@ -3,16 +3,19 @@ import './message.css';
 
 export function createMessageElement(sender, message, isAI) {
     const messageElement = document.createElement('div');
-    messageElement.className = `${isAI ? 'nai' : 'user'}-message`;
+    messageElement.className = `message ${isAI ? 'ai' : 'user'}-message`;
   
     const parsedMessage = marked(message);
   
     messageElement.innerHTML = `
-      <div class="message-text">
-        <img class="profile-picture" src="${isAI ? 'https://static-00.iconduck.com/assets.00/ai-human-icon-256x256-j1bia0vl.png' : 'https://images.vexels.com/media/users/3/137047/isolated/lists/5831a17a290077c646a48c4db78a81bb-user-profile-blue-icon.png'}" alt="${sender} Profile Picture">
-        <strong>${sender}</strong>
+      <div class="message-content">
+        ${isAI ? '' : `<img class="profile-picture" src="https://images.vexels.com/media/users/3/137047/isolated/lists/5831a17a290077c646a48c4db78a81bb-user-profile-blue-icon.png" alt="User Profile Picture">`}
+        <div class="message-text">
+          <strong class="sender-name">${sender}</strong>
+          <p>${parsedMessage}</p>
+        </div>
+        ${isAI ? `<img class="profile-picture" src="https://static-00.iconduck.com/assets.00/ai-human-icon-256x256-j1bia0vl.png" alt="AI Profile Picture">` : ''}
       </div>
-      <p>${parsedMessage}</p>
     `;
     return messageElement;
   }
