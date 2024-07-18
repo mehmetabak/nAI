682a6ace3c5a50d3f9d1c79d2e931a1e3fb010e4
memoli0 <70433403+memoli0@users.noreply.github.com>
2024-07-18 11:28:21 +0300

    as

--- components/message.js
+++ components/message.js
@@ -9,12 +9,12 @@ export function createMessageElement(sender, message, isAI) {
   
     messageElement.innerHTML = `
       <div class="message-content">
-        ${isAI ? '' : `<img class="profile-picture" src="https://images.vexels.com/media/users/3/137047/isolated/lists/5831a17a290077c646a48c4db78a81bb-user-profile-blue-icon.png" alt="User Profile Picture">`}
+        ${isAI ? `<img class="profile-picture" src="https://static-00.iconduck.com/assets.00/ai-human-icon-256x256-j1bia0vl.png" alt="AI Profile Picture">` : ''}
         <div class="message-text">
           <strong class="sender-name">${sender}</strong>
           <p>${parsedMessage}</p>
         </div>
-        ${isAI ? `<img class="profile-picture" src="https://static-00.iconduck.com/assets.00/ai-human-icon-256x256-j1bia0vl.png" alt="AI Profile Picture">` : ''}
+        ${isAI ? '' : `<img class="profile-picture" src="https://images.vexels.com/media/users/3/137047/isolated/lists/5831a17a290077c646a48c4db78a81bb-user-profile-blue-icon.png" alt="User Profile Picture">`}
       </div>
     `;
     return messageElement;
