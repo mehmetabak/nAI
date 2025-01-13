d3c06d66fcb296c06db1f7d3189a78e07d0e651a
memoli0 <70433403+memoli0@users.noreply.github.com>
2025-01-13 20:26:09 +0300

    adf

--- main.js
+++ main.js
@@ -383,7 +383,7 @@ async function generateResponse(model, originalText) {
     }
   } catch (error) {
     console.error(error);
-    appendMessage(model.label, "An error occurred while generating the response. Please try again.", true, "https://www.clipartmax.com/png/middle/113-1131250_png-file-error-icon.png");
+    appendMessage(model.label, "An error occurred while generating the response. Please try again.", true, "https://cdn-icons-png.flaticon.com/512/6378/6378000.png");
   } finally {
     hideLoadingDots(sendMessageButton, originalText);
     sendMessageButton.disabled = false;
