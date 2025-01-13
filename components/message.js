88a79e81aa0499ff4c384780d8bae329ce61e448
memoli0 <70433403+memoli0@users.noreply.github.com>
2025-01-13 19:50:59 +0300

    Update AI profile picture URL in createMessageElement function

--- components/message.js
+++ components/message.js
@@ -22,7 +22,7 @@ export function createMessageElement(sender, message, isAI) {
   
     messageElement.innerHTML = `
       <div class="message-content">
-        ${isAI ? `<img class="profile-picture" src="https://static-00.iconduck.com/assets.00/ai-human-icon-256x256-j1bia0vl.png" alt="AI Profile Picture">` : `<img class="profile-picture" src="https://images.vexels.com/media/users/3/137047/isolated/lists/5831a17a290077c646a48c4db78a81bb-user-profile-blue-icon.png" alt="User Profile Picture">`}
+        ${isAI ? `<img class="profile-picture" src="https://i.imgur.com/Y0aM4cv.png" alt="AI Profile Picture">` : `<img class="profile-picture" src="https://images.vexels.com/media/users/3/137047/isolated/lists/5831a17a290077c646a48c4db78a81bb-user-profile-blue-icon.png" alt="User Profile Picture">`}
         <div class="message-text">
           <div class="sender-name">
             ${sender}
