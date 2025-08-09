971d1ada1775f173df1085eb13d455d712c7617d
Mehmet Abak <70433403+mehmetabak@users.noreply.github.com>
2025-08-09 19:37:07 +0300

    d

--- src/App.jsx
+++ src/App.jsx
@@ -290,7 +290,7 @@ const App = () => {
         // Kısa bir gecikme, animasyonun tamamlanmasına ve DOM'un stabil hale gelmesine olanak tanır.
         const timer = setTimeout(() => {
             setPadding();
-        }, 50); // 50ms genellikle yeterlidir
+        }, 90); // 50ms genellikle yeterlidir
 
         window.addEventListener('resize', setPadding);
         
